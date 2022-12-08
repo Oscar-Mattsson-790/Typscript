cb91daa9a4025747e0e8a8e39854257b75b9cfe8
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2022-12-08 03:33:30 -0600

    complete

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
 let greeting: string = "Hello World";
-console.log(greeting);
-let booleanValue: boolean = true;
+let isGreeting: boolean = true;
+let randomNumber: number = Math.floor(Math.random() * 10);
+
+// Denna variabel kan ha vilken datatyp som helst
+let changing: any = "Hej";
+changing = 1;
+
+// Detta är betyder att names är en array med strängar i
+let names: string[] = [];
+
+names.push("Ada Lovelace");
+
+// Denna variabel kan antigen vara en sträng eller en boolean
+let twoTypes: string | boolean = "Hej";
+twoTypes = true;
+
+// lastname är en frivillig parameter i funktionen nedan och måste alltså inte få ett värdet vi
+// funktionsanropet. Detta visas med ett ? efter namnet.
+function getName(firstname: string, lastname?: string): string {
+  console.log(firstname, lastname);
+  const fullname = firstname + " " + lastname;
+  return fullname;
+}
+
+const fullname: string = getName("Ada", "Lovelace");
